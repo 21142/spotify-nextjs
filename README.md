c59cba9567df0db327f69580b02a1bfca24cb5be
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-02-28 10:53:23 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,15 @@
+## App implementing Spotify API
+
+Using Next.js + Recoil + Tailwind CSS + NextAuth
+
+---
+
+To run the development server use the following command:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Then [http://localhost:3000](http://localhost:3000) in your browser.
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <h1>This is a spotify clone</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      {/* Footer */}
+      {/* Player */}
+    </div>
+  );
+}
