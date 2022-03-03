85e3b8f9269616e151798cd8565a5a6f471f2d36
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-03-03 03:06:44 +0100

    Implement logging in with spotify using NextAuth.js and Spotify Web API

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## App implementing Spotify API
 
-Using Next.js + Recoil + Tailwind CSS + NextAuth
-
----
-
+\
+\
+In this build I used Next.js + Recoil + Tailwind CSS + NextAuth\
+\
 To run the development server use the following command:
 
 ```bash
@@ -12,4 +12,11 @@ npm run dev
 yarn dev
 ```
 
-Then open [http://localhost:3000](http://localhost:3000) in your browser.
+Then open [http://localhost:3000](http://localhost:3000) in your browser\
+\
+Docs used for this build:
+
+- [https://next-auth.js.org/tutorials/refresh-token-rotation](https://next-auth.js.org/tutorials/refresh-token-rotation)
+- [https://developer.spotify.com/documentation/general/guides/authorization/scopes/](https://developer.spotify.com/documentation/general/guides/authorization/scopes/)
+- [https://recoiljs.org/docs/introduction/getting-started/](https://recoiljs.org/docs/introduction/getting-started/)
+- [https://tailwindcss.com/docs/guides/nextjs](https://tailwindcss.com/docs/guides/nextjs)
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
+import { getProviders, signIn } from "next-auth/react";
 import React from "react";
 
-function Login() {
+function Login({ providers }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-black">
       <img
@@ -8,13 +9,28 @@ function Login() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
         alt=""
       />
-      <div className="">
-        <button className="bg-[#18D860] text-white p-4 rounded-full">
-          Login with Spotify
-        </button>
-      </div>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.key} className="">
+          <button
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            className="bg-[#18D860] text-white p-4 rounded-full"
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
