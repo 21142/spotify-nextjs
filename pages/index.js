5de11a1e329406a3673764b1ed9160939d0c171c
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-03-06 04:28:46 +0100

    Added server-sided prefetching of user's session
    Fixed an issue where there was no data send from Spotify on initial page load

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
 import Center from "../components/Center";
@@ -21,4 +22,14 @@ export default function Home() {
       {/* Player */}
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
