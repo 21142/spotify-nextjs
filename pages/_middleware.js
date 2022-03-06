514aade2e0e2b0be8199ff74bd7afe32e59e7e87
Maciej Galiński <s21142@pjwstk.edu.pl>
2022-03-06 13:45:59 +0100

    Fixed redirect to login page when user is not signed in

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -11,6 +11,6 @@ export async function middleware(req) {
   }
 
   if (!token && pathname !== "/login") {
-    return NextResponse.redirect("/login");
+    return NextResponse.redirect("http:localhost:3000/login");
   }
 }
